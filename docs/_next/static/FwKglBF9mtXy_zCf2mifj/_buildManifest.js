35b5f8ffc6cf30975bf28702271b00cc6c114fb1
Anuj0103 <yanujdhingra01@gmail.com>
2023-08-09 01:11:35 +0530

    Deploy Next.js app to GitHub Pages

---
+++ docs/_next/static/FwKglBF9mtXy_zCf2mifj/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/694-b62a3194e29f1702.js","static/chunks/pages/index-8e05814ca6e71003.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
